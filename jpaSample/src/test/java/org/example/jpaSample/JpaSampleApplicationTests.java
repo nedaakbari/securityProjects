a0c0d135b57deedabc9ccb171e70c7eb061bbab9
nedaakbari <nedaakbari555@gmail.com>
2024-03-27 00:43:14 +0330

    update

---
+++ jpaSample/src/test/java/org/example/jpaSample/JpaSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.jpaSample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
